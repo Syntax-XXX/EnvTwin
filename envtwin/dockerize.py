869e1b998d9602d484b9bea77d38c17ca347cb91
Syntax-XXX <elias.wiesinger@proton.me>
2025-06-17 11:02:41 +0200

    .

--- envtwin/dockerize.py
+++ envtwin/dockerize.py
@@ -1,3 +1,15 @@
+import subprocess
+import os
+
+def get_git_remote_url():
+    try:
+        result = subprocess.getoutput('git config --get remote.origin.url')
+        if result and result.startswith('http'):
+            return result.strip()
+    except Exception:
+        pass
+    return None
+
 def generate_dockerfile(snapshot):
     os_info = snapshot['os']
     packages = snapshot['packages']
@@ -17,5 +29,12 @@ def generate_dockerfile(snapshot):
     dockerfile.append('RUN apt-get install -y python3-pip')
     if packages['pip']:
         dockerfile.append('RUN pip3 install ' + ' '.join([pkg.split('==')[0] for pkg in packages['pip']]))
+    dockerfile.append('WORKDIR /app')
+    dockerfile.append('COPY . /app')
+    user_git_url = get_git_remote_url()
+    if user_git_url:
+        dockerfile.append(f'RUN git clone {user_git_url} /app')
+    else:
+        dockerfile.append('# RUN git clone https://github.com/yourusername/yourrepo.git /app')
     dockerfile.append('CMD ["/bin/bash"]')
     return '\n'.join(dockerfile)
